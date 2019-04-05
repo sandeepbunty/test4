2225c6a3f3d7f5283d9251096286e3b3029b54da
sandeepbunty <38014670+sandeepbunty@users.noreply.github.com>
2019-04-05 18:19:44 +0530

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,1 +1,33 @@
-lksdjklsjlkdjlkszdfasdfds
+ $("button#acctAddBtn").click(function(){
+       
+       var url = '<?php echo $this->webroot."Accounts/add/".$account["Account"]["id"] ?>';
+       
+       var data = $("form#AccountAddForm").serialize();
+         console.log(data);
+
+       die();
+
+       //hide to prevent double click
+       // $(this).hide();
+
+       $.post(url,data,function(res){
+       
+           var indexUrl = '<?php echo $this->webroot."Accounts/index/" ?>';
+           
+           if(res.success == 0){
+                alert(res.msg);
+               $("button#acctAddBtn").show();
+           }
+           else if(res.success == 1){
+               alert(res.msg)
+               
+               //redirect to index
+               indexUrl += res.id;
+               
+               window.location.href = indexUrl;
+               
+          }
+            
+       },"json");
+     
+    });
